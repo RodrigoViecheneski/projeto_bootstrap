f55e325c61bcdfc9040aa1675d1711f65dde03ed
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2026-02-24 19:47:59 -0300

    firstCommit 24-02

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="container">
+        <header class="header_area">
+            <div class="main-menu">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container">
+                        <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <div class="ms-auto"></div>
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Trabalhos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Sobre mim</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contato</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    </div>
+    <!--End Header Area-->
+    <!--Start Main Area-->
+    <main class="site-main">
+        <!--Start Banner Area-->
+        <section class="site-banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 site-title">
+                        <h3 class="title-text text-uppercase">Olá</h3>
+                        <h1 class="title-text text-uppercase">Eu sou Rodrigo</h1>
+                        <h4 class="title-text text-uppercase">Desenvolvedor Web</h4>
+                        <div class="site-buttons">
+                            <button type="button" class="btn button primary-button text-uppercase">Orçamento</button>
+                            <button type="button" class="btn button secondary-button text-uppercase">Meu CV</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 banner-image">
+                        <img src="img/banner-principal.png" alt="Banner Image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End Banner Area-->
+        <!--About Area-->
+        <section class="about-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="aboutimage">
+                            <img src="img/info.png" alt="About Image" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 about-title">
+                        <h2 class="text-uppercase pt-5">
+                            <span>Um pouco</span>
+                            <span>Sobre</span>
+                            <span>Minha história</span>
+                        </h2>
+                        <div class="paragraph py-4 w-75">
+                            <p class="parag">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus
+                                voluptate
+                                quisquam, cumque corporis deleniti, neque, magnam consequatur doloremque dicta
+                                consequatur
+                                dolorem. Voluptas, voluptate. Doloribus voluptate quisquam, cumque corporis deleniti,
+                                neque,
+                                magnam consequatur doloremque dicta consequatur dolorem.
+                            </p>
+                            <p class="parag">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus
+                                voluptate
+                                quisquam, cumque corporis deleniti, neque, magnam consequatur doloremque dicta
+                                consequatur
+                                dolorem. Voluptas, voluptate. Doloribus voluptate quisquam, cumque corporis deleniti,
+                                neque,
+                                magnam consequatur doloremque dicta consequatur dolorem.
+                            </p>
+                        </div>
+                        <button type="button" class="btn button primary-button text
+                            ">Download CV</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End About Area-->
+    </main>
+    <!--End Main Area-->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>

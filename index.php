faaaaa817ed54f50812a0c6723eef47d7625ef6a
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2026-03-02 19:07:04 -0300

    02-03

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
     <title>Bootstrap</title>
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <!--font awesome icons-->
+    <link rel="stylesheet" href="css/all.min.css">
+
+    <!-- CUstom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
+
 </head>
 
 <body>
@@ -103,16 +111,180 @@
                                 magnam consequatur doloremque dicta consequatur dolorem.
                             </p>
                         </div>
-                        <button type="button" class="btn button primary-button text
+                        <button type="button" class="btn button primary-button text-uppercase
                             ">Download CV</button>
                     </div>
                 </div>
             </div>
         </section>
         <!-- End About Area-->
+        <!-- Brand Area-->
+        <section class="brand-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-12 col-md-12">
+                        <div class="first-row row">
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p1.png" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p2.png" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p3.png" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p4.jpeg" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p6.jpeg" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p7.jpeg" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p8.png" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p4.jpeg" alt="Brand Image">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <div class="single-brand">
+                                    <img src="img/brands/p1.png" alt="Brand Image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-12 col-md-12">
+                        <div class="experience-area">
+                            <div class="d-flex flex-row years-area">
+                                <h2 class="p-3 years">10</h2>
+                                <h2>
+                                    <span>Anos</span>
+                                    <span>Experiência</span>
+                                    <span>Trabalho</span>
+                                </h2>
+                            </div>
+                            <div class="d-flex flex-row flex-wrap call-area">
+                                <span><i class="fa-solid fa-phone fa-3x px-2"></i></span>
+                                <div class="call-now">
+                                    <a href="#" class="text-uppercase h4 font-roboto">LIGAR</a>
+                                    <span class="font-roboto py-2">(42) 99984-0908</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Brand Area-->
+        <!--Services Area-->
+        <section class="services-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center services-title">
+                        <h1 class="text-uppercase title-text">Serviços Oferecidos</h1>
+                        <p class="parag">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus
+                            voluptate
+                            quisquam, cumque corporis deleniti, neque, magnam consequatur doloremque dicta
+                            consequatur
+                            dolorem. Voluptas, voluptate. Doloribus voluptate quisquam, cumque corporis deleniti,
+                            neque,
+                            magnam consequatur doloremque dicta consequatur dolorem.
+                        </p>
+
+                    </div>
+                </div>
+                <div class="container service-list">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="services">
+                                <div class="services-img text-center py-4">
+                                    <img src="img/services/s1.png" style="width: 50%;" alt="Service Image">
+                                </div>
+                                <div class="card-body text-center">
+                                    <h5 class="card-body text-center">Desenvolvimento</h5>
+                                    <p class="card-text text-secondary">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
+                                        Doloribus voluptate quisquam, cumque corporis deleniti, neque, magnam
+                                        consequatur doloremque dicta consequatur dolorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="services">
+                                <div class="services-img text-center py-4">
+                                    <img src="img/services/s2.png" style="width: 32%;" alt="Service Image">
+                                </div>
+                                <div class="card-body text-center">
+                                    <h5 class="card-body text-center">UX/UI</h5>
+                                    <p class="card-text text-secondary">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
+                                        Doloribus voluptate quisquam, cumque corporis deleniti, neque, magnam
+                                        consequatur doloremque dicta consequatur dolorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="services">
+                                <div class="services-img text-center py-4">
+                                    <img src="img/services/s3.png" style="width: 40%;" alt="Service Image">
+                                </div>
+                                <div class="card-body text-center">
+                                    <h5 class="card-body text-center">Web Design</h5>
+                                    <p class="card-text text-secondary">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
+                                        Doloribus voluptate quisquam, cumque corporis deleniti, neque, magnam
+                                        consequatur doloremque dicta consequatur dolorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="services">
+                                <div class="services-img text-center py-4">
+                                    <img src="img/services/s4.png" style="width: 40%;" alt="Service Image">
+                                </div>
+                                <div class="card-body text-center">
+                                    <h5 class="card-body text-center">SEO</h5>
+                                    <p class="card-text text-secondary">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
+                                        Doloribus voluptate quisquam, cumque corporis deleniti, neque, magnam
+                                        consequatur doloremque dicta consequatur dolorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End Services Area-->
+
     </main>
     <!--End Main Area-->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type=" text/javascript" src="js/bootstrap.min.js">
+    </script>
 </body>
 
 </html>
